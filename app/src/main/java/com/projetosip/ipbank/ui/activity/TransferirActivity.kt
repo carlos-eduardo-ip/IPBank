e29ad34bea60f9813e1b3451d02154a56b349877
carlos-eduardo-ip <carlos.eduardog2p@gmail.com>
2024-07-01 22:14:23 -0300

    Adicionando fragment_contatos e fragment_transferencia_pix

--- app/src/main/java/com/projetosip/ipbank/ui/activity/TransferirActivity.kt
+++ app/src/main/java/com/projetosip/ipbank/ui/activity/TransferirActivity.kt
@@ -1,13 +1,32 @@
 package com.projetosip.ipbank.ui.activity
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayoutMediator
 import com.projetosip.ipbank.R
+import com.projetosip.ipbank.databinding.ActivityTransferirBinding
+import com.projetosip.ipbank.ui.adapter.ViewPagerAdapter
+import com.projetosip.ipbank.ui.viewmodel.AuthViewModel
+import com.projetosip.ipbank.ui.viewmodel.factory.AuthViewModelFactory
 
 class TransferirActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityTransferirBinding.inflate( layoutInflater )
+    }
+
+    private val authViewModel: AuthViewModel by viewModels { AuthViewModelFactory(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +36,75 @@ class TransferirActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializarToolbar()
+        inicializarNevevacaoAbas()
     }
+
+    private fun inicializarNevevacaoAbas() {
+
+        val tabLayout = binding.tabLayoutTransferir
+        val viewPage = binding.viewPagerTransferir
+        //Adapter
+        val abas = listOf("PIX", "CONTATOS")
+        viewPage.adapter = ViewPagerAdapter(abas, supportFragmentManager, lifecycle)
+
+        tabLayout.isTabIndicatorFullWidth = true
+        TabLayoutMediator(tabLayout, viewPage){ aba, posicao ->
+            aba.text = abas[posicao]
+        }.attach()
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeTransferirToolbar.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Gerenciamento de Cartão"
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_principal, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    when( menuItem.itemId ){
+                        R.id.item_sobre ->{
+                            Intent(this@TransferirActivity, SobreActivity::class.java)
+                        }
+                        R.id.item_sair -> {
+                            deslogarUsuario()
+                        }
+                    }
+                    return true
+                }
+
+            }
+        )
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun deslogarUsuario() {
+        AlertDialog.Builder(this)
+            .setTitle("Deslogar")
+            .setMessage("Deseja realmente sair?")
+            .setNegativeButton("Não") { dialog, _ -> dialog.dismiss() }
+            .setPositiveButton("Sim") { _, _ ->
+                authViewModel.logout()
+            }
+            .create()
+            .show()
+    }
+
 }
